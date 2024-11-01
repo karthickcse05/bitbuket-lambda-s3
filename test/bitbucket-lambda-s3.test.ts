7c17c37f25bf55aa02fda69e927f2c7d6876d0c2
skarthick <karthick.s@tranetechnologies.com>
2024-11-01 11:19:38 +0530

    Initial commit

---
+++ test/bitbucket-lambda-s3.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BitbucketLambdaS3 from '../lib/bitbucket-lambda-s3-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/bitbucket-lambda-s3-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BitbucketLambdaS3.BitbucketLambdaS3Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
